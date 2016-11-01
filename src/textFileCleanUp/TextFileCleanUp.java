a93d117fd0194c7cde3b445b8f5d6a6d26511949
brianrichardmccarthy <brianmccarthy1@outlook.ie>
2016-11-01 23:54:19 +0000

    Changed search to binary search, + additional clean up

--- src/textFileCleanUp/TextFileCleanUp.java
+++ src/textFileCleanUp/TextFileCleanUp.java
@@ -1,4 +1,5 @@
 package textFileCleanUp;
+
 /**
  * @author Brian McCarthy
  * @verison 1.0
@@ -10,14 +11,15 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class TextFileCleanUp {
 
-	private ArrayList<String> text = new ArrayList<String>();
+	private List<String> text = new ArrayList<String>();
 	private int num = 1;
-	private int count = 0;
 	private String fileName = "text";
 	private int numOfFiles = 11;
 
@@ -25,8 +27,13 @@ public class TextFileCleanUp {
 	 * Constructor for TextFileCleanUp class.<br>
 	 * Sets the variables fileName and numOfFiles.<br>
 	 * Then calls the openFile and writeFile() methods.
-	 * @param fileName (Type: String) base name of the text files e.g text1.txt base name = text
-	 * @param numOfFiles (Type: int) number of text files e.g text1.txt, text2.txt numOfFiles = 2
+	 * 
+	 * @param fileName
+	 *            (Type: String) base name of the text files e.g text1.txt base
+	 *            name = text
+	 * @param numOfFiles
+	 *            (Type: int) number of text files e.g text1.txt, text2.txt
+	 *            numOfFiles = 2
 	 */
 	public TextFileCleanUp(String fileName, int numOfFiles) {
 		this.fileName = fileName;
@@ -42,53 +49,37 @@ public class TextFileCleanUp {
 	 * To remove any duplicates lines.<br>
 	 * 
 	 */
-	public void openFile() {
+	private void openFile() {
 		// numOfFiles = 5 num will go from 1 - 5 inclusive
 		while (num <= numOfFiles) {
 			// store the file in a object
 			// src\\textFileCleanUp\\Input\\text1.txt
 			File file = new File("src\\textFileCleanUp\\Input\\" + fileName + num + ".txt");
-			
-			// try with resources opens the file, to process it line by line in the while loop
+
+			// try with resources opens the file, to process it line by line in
+			// the while loop
 			try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-				
+
 				// used for each line in current text file
 				String line;
-				
+
 				// loops through each line in text file
 				while ((line = reader.readLine()) != null) {
-					
-					// variable to check if the current value in line has been entered into the array list
-					boolean found = false;
-					
-					// count keeps track of how many values have been entered into the array list already
-					// loops thought all the values in the array list
-					for (int x = 0; x < count; x++) {
-						
-						/*
-						 * if the the current position of text is equal to the line
-						 * e.g if text.get(0) = one and line = one
-						 * set found to true
-						 * and skip the reset of the strings in the array list
-						 * because there should only be one of each string
-						 * (done by setting the loop control variable to a value greater than count)
-						 * */
-						if (text.get(x).equals(line)) {
-							found = true;
-							x = count * 2;
-						}
-					}
-					
-					// if the current value in line was not found
-					// increment count by one and add the current value of line to the array list
-					if (!found) {
-						count++;
+
+					// sort the arraylist
+					Collections.sort(text);
+
+					// bimary search the current line to see if it is in the arraylist already
+					// if binary search returns a value less than zero add the string to the array list.
+					if (Collections.binarySearch(text, line) < 0) {
 						text.add(line);
 					}
 				}
-				
-				// increment num when all lines of the text file have been read and one copy of each has been added to the array list
-				// in order to move onto the next text file or to exit the while loop
+
+				// increment num when all lines of the text file have been read
+				// and one copy of each has been added to the array list
+				// in order to move onto the next text file or to exit the while
+				// loop
 				num++;
 			} catch (Exception e) {
 				// if one file fails the skip checking the rest
@@ -102,30 +93,28 @@ public class TextFileCleanUp {
 	/**
 	 * Writes the array list of strings to a text file
 	 */
-	public void writeFile() {
-		
+	private void writeFile() {
+
 		// creates a new File Object in the output folder
-		File file = new File("src\\textFileCleanUp\\Output\\text.txt");
-		
-		// try with resources to open BufferedWriter Object using file writer object and the file
-		try (BufferedWriter write = new BufferedWriter(new FileWriter(file))) {
-			
-			// loop control variable
-			int x = 0;
-			
-			// while x is less than array list size
-			while (x < count) {
-				
+		File file = new File("src\\textFileCleanUp\\Output\\text2.txt");
+
+		// try with resources to open BufferedWriter Object using file writer
+		// object and the file
+		try (BufferedWriter w = new BufferedWriter(new FileWriter(file))) {
+
+			for (int x = 0; x < text.size(); x++) {
+
 				// write each String in the array list to the text file
-				write.write(text.get(x));
-				
+				w.write(text.get(x));
+
 				// add a new line to the text file
 				// Basically the "\n" equivalent
-				write.newLine();
-				
-				// increment the loop control variable
-				x++;
+				w.newLine();
 			}
+
+			// close the buffer writer
+			w.close();
+			
 		} catch (IOException e) {
 			// catch any problem writing the file
 			System.out.println(e.getMessage());
@@ -133,34 +122,37 @@ public class TextFileCleanUp {
 	}
 
 	public static void main(String[] args) {
-		
-		// new Scanner object to get the base name and the number of text files
+
+		// new TextFileCleanUp("text", 4);
+
+		// new Scanner object to get the base name and the number of textfiles
 		Scanner input = new Scanner(System.in);
-		System.out.println("Put the Files into input folder and for each text file name them the same name and increment by one e.g text1.txt text2.txt etc");
+		System.out.println(
+				"Put the Files into input folder and for each text file name them the same name and increment by one e.g text1.txt text2.txt etc");
 		System.out.println("When you've finished this, please enter just the name e,g text1.txt text2.txt enter text");
-		
 		// base name for text files
 		String name = input.nextLine();
 		System.out.println("How many files are there? e.g text1.txt, text2.txt enter 2");
-		int num;
+		// number of text files
+		int num = input.nextInt();
 		try {
-			
-			// number of text files
-			num = input.nextInt();
-			
+
 			// run one new instance of TextFileCleanUp
-			// because the constructor class calls the other method to perform the task,
+			// because the constructor class calls the other method to perform
+			// the task,
 			// the new instance doesn't need to be assigned to a variable
-			// once it's done the writeFile method the object is destroyed and memory released
+			// once it's done the writeFile method the object is destroyed and
+			// memory released
 			new TextFileCleanUp(name, num);
 			System.out.println("Done");
 		} catch (InputMismatchException e) {
-			// catching anything other than a number entered
+			// catching anything other than
+			// number entered
 			System.out.println("There was a problem with the number you entered.");
-		} finally {
+		} finally { 
 			// close the scanner instance
-			input.close();			
+			input.close();
 		}
 	}
 
-} 
+}
